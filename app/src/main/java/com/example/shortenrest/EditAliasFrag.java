0fe1adb712e658eded45cae59ffa2790c4a8aa2f
linaalkhodair <linaalkhodair9@gmail.com>
2020-06-24 13:52:52 +0300

    dialog messages for edit alias

--- app/src/main/java/com/example/shortenrest/EditAliasFrag.java
+++ app/src/main/java/com/example/shortenrest/EditAliasFrag.java
@@ -1,6 +1,10 @@
 package com.example.shortenrest;
 
 
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,15 +19,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.common.base.Strings;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.Objects;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+
+
 
 
 public class EditAliasFrag extends Fragment {
@@ -38,6 +47,15 @@ public class EditAliasFrag extends Fragment {
     Credentials credentials = new Credentials();
     String domain = credentials.getDomain();
     String apiKey = credentials.getAPI_KEY();
+
+    Button okBtn,cancelBtn;
+    TextView dialogMsg;
+    private Context mContext;
+
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
 
     public EditAliasFrag() {
         // Required empty public constructor
@@ -78,13 +96,37 @@ public class EditAliasFrag extends Fragment {
             @Override
             public void onClick(View v) {
 
-                String aliasName = getAliasName(shortURL.getText().toString());
+                if (validate()) {
+                    String aliasName = getAliasName(shortURL.getText().toString());
 
-                getAlias(aliasName);
+                    getAlias(aliasName);
+                }
             }
         });
 
     } //end onViewCreated
+
+
+    public boolean validate(){
+
+        String url = shortURL.getText().toString();
+
+        //check if url is empty
+        if (url.isEmpty()) {
+            createDialog("Missing Short URL field, please fill in field and try again.");
+            return false;
+        }
+
+
+        //check if input is too short compared to domain?
+        if (url.length() < domain.length()) {
+            //create dialog
+            createDialog("Alias is not found, please enter a valid alias and try again.");
+            return false;
+        }
+
+        return true;
+    }
 
 
     public String getAliasName(String shortURL){
@@ -112,6 +154,7 @@ public class EditAliasFrag extends Fragment {
                 .addHeader("x-api-key", "e9896260-b45b-11ea-9ec4-b1aa9a0ed929") //later change take api from class
                 .addHeader("Content-Type", "application/json")
                 .build();
+
         try (Response response = client.newCall(request).execute()) {
 
             Log.d("HERE!","response is :"+response);
@@ -120,31 +163,44 @@ public class EditAliasFrag extends Fragment {
             Log.d("json","json:"+json);
             JSONObject jsonObj = new JSONObject(json);
 
-            //displaying domain
-            String domainName = jsonObj.getString("domainName");
-            domainEdit.setVisibility(View.VISIBLE);
-            domainTV.setVisibility(View.VISIBLE);
-            domainEdit.setText(domainName);
+            if (jsonObj.getString("name").isEmpty()) {
+                createDialog("Alias is not found, please enter a valid alias and try again.");
+            }
 
-            //displaying destination
-            int length = jsonObj.getJSONArray("destinations").length(); //come back...
-            Log.d("herehh","len:"+length);
-
-            JSONObject object = jsonObj.getJSONArray("destinations").getJSONObject(0);
-            String destinatonUrl = object.getString("url");
-            Log.d("tesssst", "url:" + destinatonUrl);
+            else {
 
 
-            destTV.setVisibility(View.VISIBLE);
-            destURL.setVisibility(View.VISIBLE);
-            destURL.setText(destinatonUrl);
+                //displaying domain
+                String domainName = jsonObj.getString("domainName");
+                domainEdit.setVisibility(View.VISIBLE);
+                domainTV.setVisibility(View.VISIBLE);
+                domainEdit.setText(domainName);
 
+                //displaying destination
+                int length = jsonObj.getJSONArray("destinations").length(); //come back...
+                Log.d("herehh", "len:" + length);
+
+                JSONObject object = jsonObj.getJSONArray("destinations").getJSONObject(0);
+                String destinatonUrl = object.getString("url");
+                Log.d("tesssst", "url:" + destinatonUrl);
+
+
+                destTV.setVisibility(View.VISIBLE);
+                destURL.setVisibility(View.VISIBLE);
+                destURL.setText(destinatonUrl);
+
+            } //end else
 
         } catch (IOException e) {
             e.printStackTrace();
         } catch (JSONException e) {
+
+            if (e.getMessage().equals("Value null of type org.json.JSONObject$1 cannot be converted to JSONObject")) {
+                createDialog("Alias is not found, please enter a valid alias and try again.");
+            }
             e.printStackTrace();
         }
+
 
 
     } //end getAlias
@@ -154,4 +210,30 @@ public class EditAliasFrag extends Fragment {
 
     }
 
+
+    private void createDialog(String message){
+        final Dialog dialog = new Dialog(mContext);
+        dialog.setContentView(R.layout.alert_dialog);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        okBtn=dialog.findViewById(R.id.ok_btn_alert_dialog);
+        //  cancelBtn=dialog.findViewById(R.id.cancel_btn_dialog);
+        dialogMsg=dialog.findViewById(R.id.dialog_message);
+
+
+        dialogMsg.setText(message);
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
+                                     @Override
+                                     public void onClick(View view) {
+                                         dialog.cancel();
+                                     }//end of onClick
+                                 }//end of OnClickListener
+        );
+
+
+
+        dialog.show();
+
+    }//end of createDialog
 }
